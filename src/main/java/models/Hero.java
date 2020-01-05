5aecf7d3d43099cf48d33880a0b2987c20313724
Hammertonmutuku <lthamma3@gmail.com>
2020-01-06 01:48:14 +0300

    added the index.hbs route in the App.java

--- src/main/java/models/Hero.java
+++ src/main/java/models/Hero.java
@@ -1,4 +1,14 @@
 package models;
 
 public class Hero {
+
+    private final String content;
+
+    public Hero (String content){
+        this.content = content;
+    }
+    public String getContent() {
+        return content;
+    }
+
 }
--- src/test/java/models/HeroTest.java
+++ src/test/java/models/HeroTest.java
@@ -2,6 +2,7 @@ package models;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 
 import static org.junit.Assert.*;
 
@@ -9,6 +10,19 @@ public class HeroTest {
 
     @Before
     public void setUp() throws Exception {
+    }
+
+    @Test
+    public void NewHeroObjectGetsCorrectlyCreated_true() throws Exception {
+        Hero hero = new Hero("Number 1:Superman");
+        assertEquals(true, hero instanceof Hero);
+    }
+
+    @Test
+    public void HeroInstantiatesWithContent_true() throws Exception {
+        Hero hero = new Hero("Number 1: Superman");
+        assertEquals("Number 1: Superman", hero.getContent());
+
     }
 
     @After
