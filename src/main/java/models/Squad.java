539f555be6496874d14bfe4606091e8c38da445d
Hammertonmutuku <lthamma3@gmail.com>
2020-01-06 05:48:53 +0300

    added the variables to squard

--- src/main/java/models/Squad.java
+++ src/main/java/models/Squad.java
@@ -2,4 +2,12 @@ package models;
 import java.util.ArrayList;
 import java.util.List;
 public class Squad {
+    public class Squad {
+        private int id;
+        private String name;
+        private int currentSize;
+        private int maxSize;
+        private String motive;
+        private ArrayList<Hero> heroes = new ArrayList<>();
+        private static ArrayList<Squad> squadInstance = new ArrayList<>();
 }
