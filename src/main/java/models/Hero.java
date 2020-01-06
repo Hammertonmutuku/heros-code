04a3ed2d93024a63a84eb79c06c80fd7f2369d51
Hammertonmutuku <lthamma3@gmail.com>
2020-01-06 05:00:54 +0300

    added static variables

--- src/main/java/models/Hero.java
+++ src/main/java/models/Hero.java
@@ -3,6 +3,13 @@ package models;
 import java.util.ArrayList;
 
 public class Hero {
+    private int id;
+    private String name;
+    private int age;
+    private String specialPower;
+    private String weakness;
+    private int squad;
+    private static ArrayList<Hero> heroInstance = new ArrayList<>();
 
     private final String content;
     private static ArrayList<Hero> instances= new ArrayList<>();
